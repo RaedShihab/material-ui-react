51c9f3f35d62a6d28c944e822841347d5dc5565c
RaedShihab <raedshihab92@gmail.com>
2019-12-01 04:04:12 +0200

    ui

---
+++ material-ui/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import NavBar from './component/navBar';
+
+
+
+
+function App() {
+  return (
+      <div>
+        <NavBar />
+      </div>
+  );
+}
+
+export default App;
