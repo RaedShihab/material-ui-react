57493153d4f9a0dcc57e8ba7012ddba938b9bb86
RaedShihab <raedshihab92@gmail.com>
2019-12-29 05:11:42 +0200

    adding hover to header

--- material-ui/src/components/layouts/Header.js
+++ material-ui/src/components/layouts/Header.js
@@ -1,58 +1,78 @@
 import React from 'react';
-import {makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import {Toolbar, IconButton} from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
+import {Toolbar, IconButton, Card, Link, Typography, Button, CardActionArea, CardMedia, TextField, CssBaseline, Container, makeStyles, Avatar} from '@material-ui/core';
 import LockIcon from '@material-ui/icons/Lock';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import StoreIcon from '@material-ui/icons/Store';
 import HelpIcon from '@material-ui/icons/Help';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia';
 import clsx from 'clsx';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import TextField from '@material-ui/core/TextField';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
+import CategoryIcon from '@material-ui/icons/Category';
+import AddIcon from '@material-ui/icons/Add';
+import PhoneIcon from '@material-ui/icons/Phone';
+import SearchIcon from '@material-ui/icons/Search';
 import '../../App.css';
-import { red, blue } from '@material-ui/core/colors';
-  
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+
 const useStyles = makeStyles({
+  main: {
+    borderBottom: 'solid #e0e0de 2px',
+    '&::before': {
+      content: '""',
+      display: 'block',
+      height: 4,
+      backgroundColor: '#ef3b44',
+      width: '100%',
+  },
+},
   header: {
     boxShadow: 'none',
     height: '350px',
   },
   icon: {
     marginRight: 10,
-    color: 'blue',
     textAlign: 'center',
     display: 'inline-block',
+    transition: 'color 0.3s',
+    marginRight: '20px',
+    '&:hover': {
+    color: "#eb0000",
+  }
   },
   list: {
     left: '50%',
-    position: 'absolute'
+    position: 'absolute',
   },
   butn: {
     top: '2%',
     right: '0%',
     position: 'absolute',
-    width: '22%'
+    width: '24%',
+    borderRadius: '0px 0px 25px 25px',
+    height: '40px'
   },
   card: {
     position: "absolute",
+    boxShadow: 'none',
     width: '9%',
     top: "2%",
     left: '0%'
   },
+  info : {
+    position: 'relative',
+    width: '12%',
+    left: '10%'
+  },
   coutries: {
     position: 'absolute',
-    width: '6%',
-    left: '15%',
-    border: 'gray solid 2px',
-    borderRadius: '25%'
+    width: '50%',
+    right: '0%',
+    border: '#e0e0de solid 2px',
+    borderRadius: '25%',
+    marginRight: '10px'
   },
   flag: {
     width: '45%',
@@ -87,17 +107,20 @@ const useStyles = makeStyles({
   arrIcon : {
     display: 'block',
     fontSize:'13px'
-  }
+  },
 });
   export default function ButtonAppBar() {
+    const icon = ()=> {
+      return <CategoryIcon />
+    }
     const classes = useStyles();
     return (
-      <div>
+      <div className={classes.main}>
         <div style={{position: 'relative',margin: 'auto' ,width:'70%'}}>
-        <AppBar className={classes.header} style={{}} color='white' position="static">
+        <AppBar className={classes.header}color='white' position="static">
         <Toolbar>
           <Card className={classes.card}>
-            <CardActionArea style={{boxShadow: 'none'}}>
+            <CardActionArea>
               <CardMedia
                 component="img"
                 alt="Contemplative Reptile"
@@ -106,7 +129,7 @@ const useStyles = makeStyles({
               />
             </CardActionArea>
           </Card>
-          <div>
+          <div className={classes.info}>
           <div className={classes.coutries}>
             <div className={classes.flag}>
             <img style={{width:'100%'}} src="https://jo.luqta.com/uploads/countries/pic_6_1521755044.png" />
@@ -117,37 +140,42 @@ const useStyles = makeStyles({
             </div>
           </div>
           <div className={classes.lang}>
-               <Typography variant="p">عربي</Typography>
+               <Typography style={{marginLeft: '10px'}} component="p">عربي</Typography>
+          </div>
+          <div className={classes.phon}>
+          <PhoneIcon style={{marginLeft: '10px'}} fontSize='small'/>
+          <span style={{display:'inline-block', fontSize: '12px'}}>+962799097079</span>
           </div>
           </div>
-        <Button className={classes.butn} variant="contained" color="secondary">
-        Secondary
+        <Button style={{backgroundColor: '#ef3b44'}} className={classes.butn} variant="contained" color="secondary">
+          <AddIcon fontSize='large' style={{padding: '0 10 3 0'}}/>
+        Post a Free Ad!
       </Button>
         <div className={classes.list}>
-          <div className={clsx(classes.icon)}>
+          <Link href='#' underline='none' className={clsx(classes.icon)}>
           <LockIcon/>
-          <Typography component="p" variant="p">
+          <Typography component="p">
           Login
           </Typography>
-          </div>
-          <div className={clsx(classes.icon)}>
+          </Link>
+          <Link href='#' underline='none' href='#' className={clsx(classes.icon)}>
           <HelpIcon/>
-          <Typography component="p" variant="p">
+          <Typography component="p">
           Help
          </Typography>
-          </div>
-          <div className={clsx(classes.icon)}>
+          </Link>
+          <Link href='#' underline='none' className={clsx(classes.icon)}>
           <StoreIcon/>
-          <Typography component="p" variant="p">
+          <Typography component="p">
           Shop
          </Typography>
-          </div>
-          <div className={clsx(classes.icon)}>
+          </Link>
+          <Link href='#' underline='none' className={clsx(classes.icon)}>
           <PersonAddIcon/>
-          <Typography component="p" variant="p">
+          <Typography component="p">
           Register
           </Typography>
-          </div>
+          </Link>
         </div>
         </Toolbar>
         <Card className={classes.card1}>
@@ -160,17 +188,32 @@ const useStyles = makeStyles({
           title="Contemplative Reptile"
         />
       </CardActionArea>
-      {/* <CardActions>
-          
-      </CardActions> */}
     </Card>
     <React.Fragment>
       <CssBaseline />
       <Container className={classes.container} maxWidth='md'>
       <form className={classes.form} noValidate autoComplete="off">
-      <TextField className={classes.outlined} label="Outlined" variant="outlined" />
-      <TextField className={classes.outlined} label="Outlined" variant="outlined" />
-      <TextField className={classes.outlined} label="Outlined" variant="outlined" />
+      <TextField className={classes.outlined} variant="outlined" placeholder='Keywords'/>
+      <TextField
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <Avatar style={{backgroundColor: 'blue'}}>
+            <CategoryIcon />
+            </Avatar>
+          </InputAdornment>
+        )
+      }}
+       className={classes.outlined} variant="outlined" placeholder='categories'/>
+      <TextField InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Avatar style={{backgroundColor: 'blue'}}>
+              <SearchIcon />
+              </Avatar>
+            </InputAdornment>
+          ),
+        }} className={classes.outlined}  variant="outlined" placeholder='All ceties'/>
     </form>
       </Container>
     </React.Fragment>
